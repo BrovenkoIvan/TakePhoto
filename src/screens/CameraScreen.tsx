8002b1b44850945b4f7b887f533ccbf4a98c59ba
Brovenko Ivan <brovenkoivan@gmail.com>
2021-07-21 23:25:35 +0300

    add camera to android

--- src/screens/CameraScreen.tsx
+++ src/screens/CameraScreen.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import {Platform} from 'react-native'
 import {useNavigation} from '@react-navigation/native';
 import {StackNavigationProp} from '@react-navigation/stack';
 import {RootStackParamList} from './RootStackParams';
-import {launchImageLibrary} from 'react-native-image-picker';
+import {launchImageLibrary, launchCamera} from 'react-native-image-picker';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 type cameraScreenProp = StackNavigationProp<RootStackParamList, 'Camera'>;
@@ -10,6 +11,26 @@ type cameraScreenProp = StackNavigationProp<RootStackParamList, 'Camera'>;
 const CameraScreen: React.FC = () => {
   const navigation = useNavigation<cameraScreenProp>();
   
+  const takeImage = () => {
+    launchCamera(
+      {mediaType: 'photo'},
+      (response: any) => {
+        console.log(response)
+        if (response.didCancel) {
+          console.log('User cancelled image picker');
+          navigation.navigate('Photo');
+        } else if (response.error) {
+          console.log('ImagePicker Error: ', response.error);
+          navigation.navigate('Photo');
+        } else {
+          const sourse = {uri: response.assets[0].uri};
+          storeData(sourse);
+          navigation.navigate('Photo', {reload: true});
+        }
+      },
+    );
+  };
+
   const pickImage = () => {
     launchImageLibrary(
       {mediaType: 'photo', selectionLimit: 1},
@@ -36,7 +57,7 @@ const CameraScreen: React.FC = () => {
       console.log(e);
     }
   };
-  pickImage();
+  Platform.OS ==='ios' ?  pickImage() : takeImage()
   return null;
 };
 
